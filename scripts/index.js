d28a385abcce62cb7720dc1e43b4c17e95b17f14
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-20 22:46:45 -0400

    creates colors array of rgb values and adds for loop to iterate through each square

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-// alert("Hello");
+let colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255 , 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
+
+let squares = document.querySelectorAll(".square");
+//style.background currently doesn't work in firefox. use .backgroundColor instead
+for(let i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i]
+} 
