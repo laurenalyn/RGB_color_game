94fb05db435735330935f1dc76ea2daf0d05864f
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-20 23:28:05 -0400

    adds logic for correct guess

--- scripts/index.js
+++ scripts/index.js
@@ -15,5 +15,20 @@ let colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++){
+    // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    // add click listeners to squares
+    squares[i].addEventListener("click", function(){
+        // console.log("glicked a square");
+        // grab color of clicked square
+        let clickedColor = this.style.backgroundColor;
+        // compare color to pickedColor
+        if(clickedColor === pickedColor){
+            console.log("Correct!");
+        } else {
+            console.log("Incorrect.");
+        }
+    });
 } 
+
