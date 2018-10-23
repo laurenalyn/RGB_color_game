d5e875d309255edfd37063cf351298b994f64542
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-22 22:20:00 -0400

    function that gets random number

--- scripts/index.js
+++ scripts/index.js
@@ -53,3 +53,12 @@ function generateRandomColors(num) {
     return arr
 }
 
+function randomColor() {
+    // pick a "red" from 0 - 255
+    let r = Math.floor(Math.random() * 256)
+    // pick a "green" from 0 - 255
+    let g = Math.floor(Math.random() * 256)
+    // pick a "blue" from 0 - 255
+    let b = Math.floor(Math.random() * 256)
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
