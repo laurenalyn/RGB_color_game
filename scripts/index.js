323da99b848d9f97d6c3ed0d948a20dd03bdb803
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-27 23:51:55 -0400

    stores numSquares and generated random color based on difficulty level chosen

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-let colors = generateRandomColors(6);
-let pickedColor = pickColor();
 let squares = document.querySelectorAll(".square");
 let colorDisplay = document.querySelector("#colorDisplay"); //style.background currently doesn't work in firefox. use .backgroundColor instead
 let messageDislay = document.querySelector("#message");
@@ -7,20 +5,44 @@ let h1 = document.querySelector('h1');
 let resetButton = document.querySelector("#reset");
 let easyBtn = document.querySelector("#easyBtn");
 let hardBtn = document.querySelector("#hardBtn");
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
+let pickedColor = pickColor();
 
 easyBtn.addEventListener("click", function(){
     hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i ++) {
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    h1.style.backgroundColor = "#232323";
 });
 
 hardBtn.addEventListener("click", function() {
     easyBtn.classList.remove("selected");
     hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i ++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+        }
+    h1.style.backgroundColor = "#232323";    
 });
 
 resetButton.addEventListener('click', function() {
     // generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     // pick new random color from arr
     pickedColor = pickColor();
     // change color display to match picked color
